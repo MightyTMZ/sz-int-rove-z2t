81415a234113da861b11654fbdf6b5fc56db2780
chubbybaby31 <49795066+chubbybaby31@users.noreply.github.com>
2025-07-22 16:57:33 -0400

    Set-up API credentials and token retrieval method

--- data_collection.py
+++ data_collection.py
@@ -1,1 +1,32 @@
-import numpy as np
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+key = os.environ.get("api-key")
+secret = os.environ.get("api-secret")
+
+def update_token():
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = f'grant_type=client_credentials&client_id={key}&client_secret={secret}'
+    response = requests.post('https://test.api.amadeus.com/v1/security/oauth2/token', headers=headers, data=data).json()
+    os.environ["token"] = response["access_token"]
+
+def flight_destinations():
+    headers = {
+    'Authorization': f'Bearer {os.environ.get("token")}',
+    }
+    params = {
+        'origin': 'PAR',
+        'maxPrice': '200',
+    }
+    response = requests.get('https://test.api.amadeus.com/v1/shopping/flight-destinations', params=params, headers=headers)
+    return response
+
+if __name__ == "__main__":
+    if os.environ.get("token") == None:
+        update_token()
+    print(flight_destinations()) #Currently receiving Response code 500. API may be down temporarily
+    
