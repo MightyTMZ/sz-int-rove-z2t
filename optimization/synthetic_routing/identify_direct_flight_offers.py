f5be0227fbea4b0cdb6236b1bf9dd4a63af6e81e
Tom Zhang <tommyawesome08@gmail.com>
2025-08-01 13:38:30 -0400

    identify direct vs. layover flights

---
+++ optimization/synthetic_routing/identify_direct_flight_offers.py
@@ -0,0 +1,26 @@
+# based on api_data.json
+# we aim to see if any of the flights include 1 or more flights
+# that means it will be a layover flight instead of a direct flight
+
+import json
+
+with open('api_data.json') as file:
+    data = json.load(file)
+    
+    flight_offers = data['BOS-LAX']['2025-08-23']['data']
+
+    # print(flight_offers)
+
+    layover_flights = []
+
+    for x in range(len(flight_offers)):
+
+        flight = flight_offers[x]
+
+        if len(flight['itineraries']) == 1: # only 1 thing in the list of itinararies
+            layover_flights.append(flight)
+    
+    print("Total direct flights:", len(layover_flights))
+
+
+# ran it and it 
