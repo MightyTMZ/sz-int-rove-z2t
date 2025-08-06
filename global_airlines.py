e576ee51a8e3652a418ed99ee4d80667fb41e4b1
Tom Zhang <tommyawesome08@gmail.com>
2025-08-06 18:41:14 -0400

    global airlines (3 major partnerships)

---
+++ global_airlines.py
@@ -0,0 +1,370 @@
+ROVE_TRANSFER_PARTNERS = [
+    "Air France-KLM Flying Blue",
+    "Air India Maharaja Club",
+    "Accor Live Limitless (Hotels)",
+    "Aeromexico Rewards",
+    "Cathay Pacific Asia Miles",
+    "Etihad Guest",
+    "Finnair Plus",
+    "Hainan Airlines Fortune Wings Club",
+    "Vietnam Airlines Lotusmiles",
+    "Qatar Airways Privilege Club",
+    "Thai Airways Royal Orchid Plus",
+    "Turkish Airlines Miles&Smiles",
+]
+
+# https://www.staralliance.com/en/members
+STAR_ALLIANCE_PARTNERS = [
+    {
+        "name": "Air Canada",
+        "frequent flyer programme": "Aeroplan",
+        "airports": 218,
+        "aircraft": 397,
+    },
+    {
+        "name": "Aegean Airlines",
+        "frequent flyer programme": "Miles+Bonus",
+        "airports": 134,
+        "aircraft": 49,
+    },
+    {
+        "name": "Air China",
+        "frequent flyer programme": "PhoenixMiles",
+        "airports": 189,
+        "aircraft": 397,
+    },
+    {
+        "name": "Air India",
+        "frequent flyer programme": "Flying Returns",
+        "airports": 123,
+        "aircraft": 127,
+    },
+    {
+        "name": "Air New Zealand",
+        "frequent flyer programme": "Airpoints",
+        "airports": 51,
+        "aircraft": 113,
+    },
+    {
+        "name": "ANA",  # Japan's airline
+        "frequent flyer programme": "ANA Mileage Club",
+        "airports": 98,
+        "aircraft": 266,
+    },
+    {
+        "name": "Asiana Airlines",
+        "frequent flyer programme": "Asiana Club",
+        "airports": 76,
+        "aircraft": 84,
+    },
+    {
+        "name": "Austrian Airlines",
+        "frequent flyer programme": "Miles & More",
+        "airports": 130,
+        "aircraft": 83,
+    },
+    {
+        "name": "Avianca",
+        "frequent flyer programme": "LifeMiles",
+        "airports": 102,
+        "aircraft": 190,
+    },
+    {
+        "name": "Brussels Airlines",
+        "frequent flyer programme": "Miles & More",
+        "airports": 98,
+        "aircraft": 38,
+    },
+    {
+        "name": "Copa Airlines",
+        "frequent flyer programme": "ConnectMiles",
+        "airports": 81,
+        "aircraft": 104,
+    },
+    {
+        "name": "Croatia Airlines",
+        "frequent flyer programme": "Miles & More",
+        "airports": 39,
+        "aircraft": 12,
+    },
+    {
+        "name": "EGYPTAIR",
+        "frequent flyer programme": "EGYPTAIR Plus",
+        "airports": 72,
+        "aircraft": 69,
+    },
+    {
+        "name": "Ethiopian Airlines",
+        "frequent flyer programme": "ShebaMiles",
+        "airports": 126,
+        "aircraft": 108,
+    },
+    {
+        "name": "EVA Air",
+        # "frequent flyer programme": "",
+        "airports": 58,
+        "aircraft": 79,
+        "countries_served": 18,
+    },
+    {
+        "name": "LOT Polish Airlines",
+        "frequent flyer programme": "Miles & More",
+        "airports": 90,
+        "aircraft": 75,
+    },
+    {
+        "name": "Lufthansa",
+        "frequent flyer programme": "Miles & More",
+        "airports": 250,
+        "aircraft": 387,
+    },
+    {
+        "name": "Shenzhen Airlines",
+        "frequent flyer programme": "PhoenixMiles",
+        "airports": 84,
+        "aircraft": 215,
+    },
+    {
+        "name": "Singapore Airlines",
+        "frequent flyer programme": "KrisFlyer",
+        "airports": 63,
+        "aircraft": 112,
+    },
+    {
+        "name": "South African Airways",
+        "frequent flyer programme": "SAA Voyager",
+        "airports": 32,
+        "aircraft": 46,
+    },
+    {
+        "name": "Swiss",
+        "frequent flyer programme": "Miles & More",
+        "airports": 99,
+        "aircraft": 90,
+    },
+    {
+        "name": "TAP Air Portugal",
+        "frequent flyer programme": "TAP Miles&Go",
+        "airports": 85,
+        "aircraft": 99,
+    },
+    {
+        "name": "Thai Airways International",
+        "frequent flyer programme": "Royal Orchid Plus",
+        "airports": 76,
+        "aircraft": 102,
+    },
+    {
+        "name": "Turkish Airlines",
+        "frequent flyer programme": "Turkish Airlines",
+        "airports": 311,
+        "aircraft": 335,
+    },
+    {
+        "name": "United",
+        "frequent flyer programme": "MileagePlus",
+        "airports": 353,
+        "aircraft": 1329,
+    },
+]
+
+# https://www.oneworld.com/members
+ONEWORLD_ALLIANCE_PARTNERS = [
+    {
+        "name": "Alaska Airlines",
+        "frequent flyer programme": "Mileage Plan",
+        "destinations": 125,
+        "countries_territories": 7,
+    },
+    {
+        "name": "American Airlines",
+        "frequent flyer programme": "AAdvantage",
+        "destinations": 352,
+        "countries_territories": 60,
+    },
+    {
+        "name": "British Airways",
+        "frequent flyer programme": "The British Airways Club",
+        "destinations": 198,
+        "countries_territories": 72,
+    },
+    {
+        "name": "Cathay Pacific",
+        "frequent flyer programme": "Cathay",
+        "destinations": 80,
+        "countries_territories": 28,
+    },
+    {
+        "name": "Fiji Airways",
+        "frequent flyer programme": "American Airlines AAdvantage",
+        "destinations": 25,
+        "countries_territories": 14,
+    },
+    {
+        "name": "Finnair",
+        "frequent flyer programme": "Finnair Plus",
+        "destinations": 100,
+        "countries_territories": 35,
+    },
+    {
+        "name": "Iberia",  # Spain's airline
+        "frequent flyer programme": "Iberia Plus",
+        "destinations": 137,
+        "countries_territories": 46,
+    },
+    {
+        "name": "Japan Airlines",
+        "frequent flyer programme": "JGC/JMB",
+        "destinations": 199,
+        "countries_territories": 64,
+    },
+    {
+        "name": "Malaysia Airlines",
+        "frequent flyer programme": "Enrich",
+        "destinations": 55,
+        "countries_territories": 20,
+    },
+    {
+        "name": "Oman Air",
+        "frequent flyer programme": "Sindbad",
+        "destinations": 42,
+        "countries_territories": 22,
+    },
+    {
+        "name": "Qantas",
+        "frequent flyer programme": "Qantas Frequent Flyer",
+        "destinations": 98,
+        "countries_territories": 25,
+    },
+    {
+        "name": "Qatar Airways",
+        "frequent flyer programme": "Privilege Club",
+        "destinations": 178,
+        "countries_territories": 82,
+    },
+    {
+        "name": "",
+        "frequent flyer programme": "",
+        "destinations": 0,
+        "countries_territories": 0,
+    },
+    {
+        "name": "Royal Air Maroc",
+        "frequent flyer programme": "Safar Flyer",
+        "destinations": 82,
+        "countries_territories": 41,
+    },
+    {
+        "name": "Royal Jordanian",
+        "frequent flyer programme": "Royal Club",
+        "destinations": 45,
+        "countries_territories": 29,
+    },
+    {
+        "name": "SriLankan Airlines",
+        "frequent flyer programme": "FlySmiLes",
+        "destinations": 112,
+        "countries_territories": 58,
+    },
+]
+
+# https://www.skyteam.com/en/about/our-members
+SKYTEAM_ALLIANCE_PARTNERS = [
+    {
+        "name": "Aerolíneas Argentinas",
+        "frequent flyer programme": "Aerolíneas Plus",
+        "destinations": 56,
+        "countries_territories": 13,
+    },
+    {
+        "name": "Aeromexico",
+        "frequent flyer programme": "Aeromexico Rewards",
+        "destinations": 89,
+        "countries_territories": 22,
+    },
+    {
+        "name": "Air Europa",
+        "frequent flyer programme": "SUMA",
+        "destinations": 52,
+        "countries_territories": 28,
+    },
+    {
+        "name": "Air France",
+        "frequent flyer programme": "Flying Blue",
+        "destinations": 200,
+        "countries_territories": 75,
+    },
+    {
+        "name": "China Airlines",
+        "frequent flyer programme": "Dynasty Flyer",
+        "destinations": 192,
+        "countries_territories": 29,
+    },
+    {
+        "name": "China Eastern",
+        "frequent flyer programme": "Eastern Miles",
+        "destinations": 244,
+        "countries_territories": 31,
+    },
+    {
+        "name": "Delta Airlines",
+        "frequent flyer programme": "SkyMiles",
+        "destinations": 301,
+        "countries_territories": 54,
+    },
+    {
+        "name": "Garuda Indonesia",
+        "frequent flyer programme": "Garuda Miles",
+        "destinations": 52,
+        "countries_territories": 12,
+    },
+    {
+        "name": "Kenya Airways",
+        "frequent flyer programme": "Asante Rewards",
+        "destinations": 45,
+        "countries_territories": 37,
+    },
+    {
+        "name": "Korean Air",
+        "frequent flyer programme": "SKYPASS",
+        "destinations": 110,
+        "countries_territories": 39,
+    },
+    {
+        "name": "Middle East Airlines",
+        "frequent flyer programme": "Cedar Miles",
+        "destinations": 32,
+        "countries_territories": 23,
+    },
+    {
+        "name": "Saudia",
+        "frequent flyer programme": "AlFursan",
+        "destinations": 98,
+        "countries_territories": 40,
+    },
+    {
+        "name": "TAROM",
+        "frequent flyer programme": "Flying Blue",
+        "destinations": 27,
+        "countries_territories": 19,
+    },
+    {
+        "name": "Vietnam Airlines",
+        "frequent flyer programme": "Lotusmiles",
+        "destinations": 53,
+        "countries_territories": 17,
+    },
+    {
+        "name": "Virgin Atlantic",
+        "frequent flyer programme": "Flying Club",
+        "destinations": 56,
+        "countries_territories": 13,
+    },
+    {
+        "name": "XiamenAir",
+        "frequent flyer programme": "Egret Club",
+        "destinations": 95,
+        "countries_territories": 15,
+    },
+    
+]
